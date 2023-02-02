964c218c1c434601eea7cbfb8eeae692c8e296bf
Moab Santos <moabsantos.msoftware@gmail.com>
2023-02-02 17:07:06 -0300

    feat: config PM2

--- src/_config/database.module.ts
+++ src/_config/database.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-/*
-Removendo devido limitação do PM2
-import * as dotenv from 'dotenv';
-dotenv.config();
-*/
 const path = require('path'); 
-require('dotenv').config({ path: path.join(__dirname, '.env') });
+import * as dotenv from 'dotenv';
+dotenv.config({ path: path.join(__dirname, '.env') });
 
-console.log('LOCAL ENV ' + path.join(__dirname, '.env'));
 
 const configDb = {
   type: process.env.DB_TYPE as any,
